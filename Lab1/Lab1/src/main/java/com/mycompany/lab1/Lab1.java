2d0f0b096c961599d5d50b9201fdd26f0300095f
Huy <>
2024-04-20 10:28:44 +0700

    removed debug code

--- Lab1/Lab1/src/main/java/com/mycompany/lab1/Lab1.java
+++ Lab1/Lab1/src/main/java/com/mycompany/lab1/Lab1.java
@@ -1,32 +1,31 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.lab1;
-
 
 /**
  *
  * @author Student
  */
 public class Lab1 {
-     public static void main(String[] args) {
+
+    public static void main(String[] args) {
         Question1 q1 = new Question1();
         q1.Input();
         System.out.println(q1.SumFirstLastDigit());
-         
+
         Question2 q2 = new Question2();
         q2.Input();
         System.out.println(q2.MinimumNumber());
-        
+
         Question3 q3 = new Question3();
         q3.Input();
         q3.HailstoneSequence();
-        
+
         Question4 q4 = new Question4();
         q4.Input();
         System.out.println(q4.SumEvenNumbers());
-       
+
         Question5 q5 = new Question5();
         q5.Input();
         System.out.println(q5.RemoveFirstSpecific(5));
@@ -34,13 +33,13 @@ public class Lab1 {
         q5.OutputArray(q5.FindDuplicate());
         q5.RemoveDuplicate();
         q5.Output();
-        
+
         Question6 q6 = new Question6();
         q6.Input();
         System.out.println(q6.getLength());
         System.out.println(q6.getWords());
         System.out.println(q6.concat(" alo123"));
         System.out.println(q6.isPalindrome());
-        
+
     }
 }
--- Lab1/Lab1/src/main/java/com/mycompany/lab1/Question5.java
+++ Lab1/Lab1/src/main/java/com/mycompany/lab1/Question5.java
@@ -12,54 +12,58 @@ import java.util.Scanner;
  * @author Student
  */
 public class Question5 {
+
     private ArrayList<Integer> arrlist = new ArrayList<>();
-    
+
     public Question5() {
     }
 
     public Question5(ArrayList<Integer> arrlist) {
         this.arrlist = arrlist;
     }
-    
-    public boolean RemoveFirstSpecific(int n){
-        if (arrlist.contains(n)==false){
+
+    public boolean RemoveFirstSpecific(int n) {
+        if (arrlist.contains(n) == false) {
             return false;
         }
-        for (int i = 0; i < arrlist.size();i++){
-            if (n == arrlist.get(i)){
+        for (int i = 0; i < arrlist.size(); i++) {
+            if (n == arrlist.get(i)) {
                 arrlist.remove(i);
                 break;
             }
         }
         return true;
     }
-    public void InsertToPosition(int n, int pos){
+
+    public void InsertToPosition(int n, int pos) {
         int curr = arrlist.get(pos);
         arrlist.remove(pos);
-        arrlist.add(pos,n);
-        arrlist.remove(arrlist.size()-1);
+        arrlist.add(pos, n);
+        arrlist.remove(arrlist.size() - 1);
         arrlist.add(curr);
     }
-    private int FindMaxValue(){
+
+    private int FindMaxValue() {
         int max = arrlist.get(0);
-        for (int i = 0; i<arrlist.size();i++){
-            if (arrlist.get(i)>max){
+        for (int i = 0; i < arrlist.size(); i++) {
+            if (arrlist.get(i) > max) {
                 max = arrlist.get(i);
             }
         }
         return max;
     }
-    public ArrayList FindDuplicate(){
+
+    public ArrayList FindDuplicate() {
         ArrayList<Integer> duplicate = new ArrayList<>();
-        int[] counter = new int[FindMaxValue()+1];
-        for (int i = 0; i<arrlist.size();i++){
+        int[] counter = new int[FindMaxValue() + 1];
+        for (int i = 0; i < arrlist.size(); i++) {
             counter[arrlist.get(i)] += 1;
             //System.out.println(arrlist.get(i)+"="+counter[arrlist.get(i)]);
         }
-        
-        for (int i = 0;i<counter.length;i++){
-            if (counter[i] > 1){
-                for (int v = 1;v<=counter[i]-1;v++){
+
+        for (int i = 0; i < counter.length; i++) {
+            if (counter[i] > 1) {
+                for (int v = 1; v <= counter[i] - 1; v++) {
                     duplicate.add(i);
                 }
                 //System.out.println(i+"="+counter[i]);
@@ -68,33 +72,36 @@ public class Question5 {
         //OutputArray(duplicate);
         return duplicate;
     }
-    
-    public void RemoveDuplicate(){
+
+    public void RemoveDuplicate() {
         ArrayList<Integer> duplicate = FindDuplicate();
-        for (int i = 0; i<duplicate.size();i++){
+        for (int i = 0; i < duplicate.size(); i++) {
             arrlist.remove(arrlist.indexOf(duplicate.get(i)));
         }
         //OutputArray(arrlist);
     }
-    
+
     Scanner sc = new Scanner(System.in);
-    public void Input(){
+
+    public void Input() {
         System.out.print("Input size of array: ");
         int n = sc.nextInt();
-        for (int i = 0; i<n;i++){
+        for (int i = 0; i < n; i++) {
             System.out.print("Input number: ");
             arrlist.add(sc.nextInt());
         }
-        
+
     }
-    public void Output(){
-        for (int i = 0; i<arrlist.size();i++){
-            System.out.print(arrlist.get(i)+" ");
+
+    public void Output() {
+        for (int i = 0; i < arrlist.size(); i++) {
+            System.out.print(arrlist.get(i) + " ");
         }
     }
-    public void OutputArray(ArrayList arr){
-        for (int i = 0; i<arr.size();i++){
-            System.out.print(arr.get(i)+" ");
+
+    public void OutputArray(ArrayList arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
         }
     }
 }
